579656c663540b98c28abcec143f13cede1dfb59
Gottfried JACQUET <gottfried.jacquet@socotec.com>
2026-04-27 13:36:13 +0200

    [CHANGE] change the code to make the runtest

--- run_test_main.py
+++ run_test_main.py
@@ -11,7 +11,7 @@ from src.wind_turbine_analytics.application import (
 
 DEFAULT_RUNTEST_ROOT = "./experiments/scada_analyse"
 DEFAULT_TEMPLATE_PATH = "./assets/templates/template_runtest_new.docx"
-DEFAULT_OUTPUT_PATH = "./output/scada_output.docx"
+DEFAULT_OUTPUT_PATH = "./output/runtest_output.docx"
 
 
 if __name__ == "__main__":
